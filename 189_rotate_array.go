9ac260de856c817aea1624b798c76b226beea35a
Moulik Singh <mouliksingh1@gmail.com>
2024-07-16 20:01:13 +0530

    Create 189_rotate_array.go

---
+++ 189_rotate_array.go
@@ -0,0 +1,33 @@
+/* 189 ROTATE ARRAY
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+func rotate(nums []int, k int) {
+	n := len(nums)
+	k %= n
+	reverse := func(i, j int) {
+		for ; i < j; i, j = i+1, j-1 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	reverse(0, n-1)
+	reverse(0, k-1)
+	reverse(k, n-1)
+}
